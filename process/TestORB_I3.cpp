bec1025317bf8238a8c84a3056189b2ca5e56539
Igor Popov <popov.nirvana@gmail.com>
2023-01-07 15:18:45 +0300

    Fix fixed point classes

--- process/TestORB_I3.cpp
+++ process/TestORB_I3.cpp
@@ -169,6 +169,84 @@ TYPED_TEST (TestORB_I3, MultiInherit)
 		release (p3);
 	}
 
+	{ // Value type
+		V2_var v = V2_factory::_factory->create ();
+		v->val_v2 (1234);
+		{
+			V1_var out, inout = V1::_duplicate (v);
+			V1_var ret = V1::_duplicate (p->value_op (v, out, inout));
+			V2_var v2 = V2::_duplicate (V2::_downcast (ret));
+			ASSERT_TRUE (v2);
+			EXPECT_EQ (v2->val_v2 (), v->val_v2 ());
+		}
+
+		{ // Abstract base
+			A1_ptr a1 = v;
+			AbstractBase_ptr ab = a1;
+			ASSERT_TRUE (ab);
+			EXPECT_FALSE (ab->_to_object ());
+			ValueBase_var vb = ab->_to_value ();
+			ASSERT_TRUE (vb);
+			V1_ptr v1 = V1::_downcast (vb);
+			EXPECT_TRUE (v1);
+			V2_ptr v2 = V2::_downcast (vb);
+			EXPECT_TRUE (v2);
+		}
+
+		A1_var out, inout (v);
+		A1_var ret = p->abstract_op (v, out, inout);
+		V2_ptr v2 = V2::_downcast (ret->_to_value ());
+		EXPECT_TRUE (v2);
+
+	}
+
+	{ // Fixed-length struct
+		StructFixLen in, out, inout;
+		in.f = fixed_const;
+		StructFixLen ret = p->fix_struct_op (in, out, inout);
+	}
+
+	{ // Array
+		FixStructArray out, inout;
+		FixStructArray ret = p->fix_struct_array_op (FixStructArray (), out, inout);
+	}
+
+	{ // Value box
+		StringValue_var out = new StringValue ("this text will be lost"),
+			inout = new StringValue ("inout string");
+		StringValue_var in = new StringValue ("in string");
+		StringValue_var ret = p->box_op (in, out, inout);
+		EXPECT_EQ (ret->_value (), "inout string");
+		EXPECT_EQ (out->_value (), "in string");
+		EXPECT_EQ (inout->_value (), "in string");
+	}
+
+	{ // Union
+		U in, out, inout;
+		out.z ("this text will be lost");
+		inout.z ("inout string");
+		in.z ("in string");
+		U ret = p->union_op (in, out, inout);
+		EXPECT_EQ (ret.z (), "inout string");
+		EXPECT_EQ (out.z (), "in string");
+		EXPECT_EQ (inout.z (), "in string");
+	}
+
+	{ // boolean
+		bool out, inout = true;
+		bool ret = p->bool_op (false, out, inout);
+		EXPECT_TRUE (ret);
+		EXPECT_FALSE (out);
+		EXPECT_FALSE (inout);
+	}
+
+	{ // Fixed
+		Fixed_8_2 out (1.5), inout (3.5);
+		Fixed_8_2 ret = p->fixed_op (Fixed_8_2 (const_neg_exp), out, inout);
+		EXPECT_EQ (ret, Fixed_8_2 (3.5));
+		EXPECT_EQ (out, Fixed_8_2 (const_neg_exp));
+		EXPECT_EQ (inout, Fixed_8_2 (const_neg_exp));
+	}
 	// release (p) must be called automatically.
 }
 
@@ -306,10 +384,10 @@ TYPED_TEST (TestORB_I3, MultiInherit)
 
 	{ // Fixed
 		Fixed_8_2 out (1.5), inout (3.5);
-		Fixed_8_2 ret = p->fixed_op (const_neg_exp, out, inout);
-		EXPECT_EQ (ret, 3.5);
-		EXPECT_EQ (out, const_neg_exp);
-		EXPECT_EQ (inout, const_neg_exp);
+		Fixed_8_2 ret = p->fixed_op (Fixed (const_neg_exp), out, inout);
+		EXPECT_EQ (ret, Fixed_8_2 (3.5));
+		EXPECT_EQ (out, Fixed_8_2 (const_neg_exp));
+		EXPECT_EQ (inout, Fixed_8_2 (const_neg_exp));
 	}
 }
 
