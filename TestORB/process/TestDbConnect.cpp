17b6d2052bb81eb4144fcfb887456f77c8d695d7
Igor Popov <popov.nirvana@gmail.com>
2025-02-04 17:31:59 +0300

    TestDbConnect.cpp

---
+++ TestORB/process/TestDbConnect.cpp
@@ -0,0 +1,186 @@
+/*
+* Nirvana test suite.
+*
+* This is a part of the Nirvana project.
+*
+* Author: Igor Popov
+*
+* Copyright (c) 2025 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
+#include "pch.h"
+#include <gtest/gtest.h>
+#include <random>
+#include <deque>
+#include <Nirvana/File.h>
+#include <Nirvana/posix_defs.h>
+#include "IDL/DbConnect.h"
+
+namespace TestORB
+{
+
+class TestDbConnect : public ::testing::TestWithParam<Test::DbConnectFactory::Implementation>
+{
+public:
+	virtual void SetUp ()
+	{
+		// Code here will be called immediately after the constructor (right
+		// before each test).
+		auto ns = CosNaming::NamingContextExt::_narrow (CORBA::the_orb->resolve_initial_references ("NameService"));
+		ASSERT_TRUE (ns);
+		auto tmp_dir = Nirvana::Dir::_narrow (ns->resolve_str ("/var/tmp"));
+
+		// Create temporary file
+		const char PATTERN [] = "testXXXXXX.db";
+		IDL::String file_name = PATTERN;
+		file_ = tmp_dir->mkostemps (file_name, 3, O_DIRECT, 0)->file ();
+		url_ = "file:/var/tmp/" + file_name + "?mode=rwc&journal_mode=WAL";
+	}
+
+	virtual void TearDown ()
+	{
+		// Code here will be called immediately after each test (right
+		// before the destructor).
+
+		file_->remove ();
+	}
+
+	std::string random_string ();
+
+protected:
+	std::string url_;
+	std::mt19937 rndgen_;
+
+private:
+	Nirvana::File::_ref_type file_;
+};
+
+INSTANTIATE_TEST_SUITE_P (Implementations, TestDbConnect, testing::Values (
+	Test::DbConnectFactory::Implementation::SingleConnection,
+	Test::DbConnectFactory::Implementation::ConnectionPool));
+
+std::string TestDbConnect::random_string ()
+{
+	size_t len = std::uniform_int_distribution <size_t> (0, 1024) (rndgen_);
+	std::string s;
+	s.reserve (len);
+	for (; len; --len) {
+		s.push_back ((char)std::uniform_int_distribution <int> (' ', '~') (rndgen_));
+	}
+	return s;
+}
+
+TEST_P (TestDbConnect, Random)
+{
+	auto obj = ::Test::db_connect_factory->create (GetParam (), SQLite::driver, url_, "", "");
+
+	typedef ::Test::AMI_DbConnectPoller Poller;
+
+	enum class Operation
+	{
+		Set, Del, Select
+	};
+
+	struct Request
+	{
+		Poller::_ref_type poller;
+		Operation op;
+	};
+
+	std::deque <Request> active_requests;
+
+	std::bernoulli_distribution dist_write (0.2);
+	std::bernoulli_distribution dist_set (0.5);
+	std::uniform_int_distribution <int32_t> dist_id (1, 10000);
+	int iterations = std::min (std::numeric_limits <int>::max (), 10);
+	for (int i = 0; i < iterations; ++i) {
+		Operation op;
+		int32_t id;
+		if (dist_write (rndgen_)) {
+			if (dist_set (rndgen_))
+				op = Operation::Set;
+			else
+				op = Operation::Del;
+			id = dist_id (rndgen_);
+		} else
+			op = Operation::Select;
+
+		Request rq;
+		rq.op = op;
+		switch (op) {
+			case Operation::Set:
+				rq.poller = obj->sendp_set (id, random_string ());
+				break;
+			case Operation::Del:
+				rq.poller = obj->sendp_del (id);
+				break;
+			case Operation::Select:
+				rq.poller = obj->sendp_select ();
+				break;
+		}
+
+		for (auto it = active_requests.begin (); it != active_requests.end ();) {
+			if (it->poller->is_ready (0)) {
+				Request rq = std::move (*it);
+				it = active_requests.erase (it);
+				switch (it->op) {
+					case Operation::Set:
+						rq.poller->set (0);
+						break;
+					case Operation::Del:
+						rq.poller->del (0);
+						break;
+					case Operation::Select:
+					{
+						NDBC::ResultSet::_ref_type rs;
+						rq.poller->select (0, rs);
+						while (rs->next ()) {
+							;
+						}
+					} break;
+				}
+			} else
+				++it;
+		}
+
+		active_requests.push_back (std::move (rq));
+	}
+
+	while (!active_requests.empty ()) {
+		Request rq = std::move (active_requests.front ());
+		active_requests.pop_front ();
+		switch (rq.op) {
+			case Operation::Set:
+				rq.poller->set (std::numeric_limits <uint32_t>::max ());
+				break;
+			case Operation::Del:
+				rq.poller->del (std::numeric_limits <uint32_t>::max ());
+				break;
+			case Operation::Select:
+			{
+				NDBC::ResultSet::_ref_type rs;
+				rq.poller->select (std::numeric_limits <uint32_t>::max (), rs);
+				while (rs->next ()) {
+					;
+				}
+			} break;
+		}
+	}
+}
+
+}
