90f2b32cbdd37aa9d03d489ab89d8c165e832c74
Igor Popov <popov.nirvana@gmail.com>
2025-08-30 17:41:10 +0300

    Build scripts

--- TestORB.ps1
+++ TestORB.ps1
@@ -1,2 +1,4 @@
-cmake -B build\TestORB -S TestORB --toolchain "$ENV:NIRVANA_SDK\cmake\nirvana.cmake" -G Ninja -DNIRVANA_TARGET_PLATFORM=x64
+cmake -B build\TestORB -S TestORB --toolchain "$ENV:NIRVANA_SDK\cmake\nirvana.cmake" -G Ninja `
+ -DNIRVANA_TARGET_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Debug
+
 cmake --build build\TestORB
