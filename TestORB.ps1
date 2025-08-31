696008daa90ea88c45db3daed8c4df4d9a5926c5
Igor Popov <popov.nirvana@gmail.com>
2025-08-31 21:48:12 +0300

    Delete file

--- TestORB.ps1
+++
@@ -1,4 +0,0 @@
-cmake -B build\TestORB -S TestORB --toolchain "$ENV:NIRVANA_SDK\cmake\nirvana.cmake" -G Ninja `
- -DNIRVANA_TARGET_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Debug
-
-cmake --build build\TestORB
