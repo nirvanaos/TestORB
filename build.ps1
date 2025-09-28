ebce00d1016ab12d3183243d4864878bf95fd951
Igor Popov <popov.nirvana@gmail.com>
2025-09-28 19:30:25 +0300

    Add Release configuration

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
 cmake --preset=x64
-cmake --preset=x86
 cmake --build --preset=x64-debug
+cmake --build --preset=x64-release
+cmake --preset=x86
 cmake --build --preset=x86-debug
+cmake --build --preset=x86-release
