f035ee0b194284d3fad91e6dc699ce8290ac9319
Igor Popov <popov.nirvana@gmail.com>
2025-10-10 15:18:32 +0300

    Buld script

--- build_all.ps1
+++ build_all.ps1
@@ -1,4 +1,27 @@
+$failed = $false
+
 & .\build.ps1 Console
+if ($LASTEXITCODE -ne 0) {
+	$failed = $true
+}
+
 & .\build.ps1 TestFixed
+if ($LASTEXITCODE -ne 0) {
+	$failed = $true
+}
+
 & .\build.ps1 TestORB
+if ($LASTEXITCODE -ne 0) {
+	$failed = $true
+}
+
 & .\build.ps1 TestSystem
+if ($LASTEXITCODE -ne 0) {
+	$failed = $true
+}
+
+if ($failed) {
+	Write-Host "Failed"
+	exit -1;
+}
+exit 0
