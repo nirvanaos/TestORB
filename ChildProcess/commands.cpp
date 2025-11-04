5aa06acbcc3892acd907f752d703528f88b052f7
Igor Popov <popov.nirvana@gmail.com>
2025-11-04 19:06:02 +0300

    + TestShell

--- ChildProcess/commands.cpp
+++ ChildProcess/commands.cpp
@@ -9,9 +9,7 @@ extern "C" {
 
 void cmd_abort ()
 {
-	Bridge <Nirvana::POSIX>* br = static_cast <Bridge <Nirvana::POSIX>*> (
-		&static_cast <I_ptr <Nirvana::POSIX>> (Nirvana::the_posix));
-	br->_epv ().epv.raise (br, SIGABRT, nullptr);
+	Nirvana::the_posix->raise (SIGABRT);
 	NIRVANA_UNREACHABLE_CODE ();
 }
 
---
+++ TestSystem/TestShell.cpp
@@ -0,0 +1,70 @@
+/*
+* Nirvana test suite.
+*
+* This is a part of the Nirvana project.
+*
+* Author: Igor Popov
+*
+* Copyright (c) 2021 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
+#include <Nirvana/Nirvana.h>
+#include <gtest/gtest.h>
+#include <Nirvana/Shell.h>
+
+using namespace Nirvana;
+
+namespace TestShell {
+
+// Test for the Nirvana process API.
+class TestShell :
+	public ::testing::Test
+{
+protected:
+	TestShell ()
+	{}
+
+	virtual ~TestShell ()
+	{}
+
+	// If the constructor and destructor are not enough for setting up
+	// and cleaning up each test, you can define the following methods:
+
+	virtual void SetUp ()
+	{
+		// Code here will be called immediately after the constructor (right
+		// before each test).
+	}
+
+	virtual void TearDown ()
+	{
+		// Code here will be called immediately after each test (right
+		// before the destructor).
+	}
+
+};
+
+TEST_F (TestShell, Spawn)
+{
+	SpawnFiles files;
+	the_shell->get_spawn_files (files);
+	int ret = the_shell->spawn (StringSeq { "ChildProcess.nex" }, files);
+	EXPECT_EQ (ret, 0);
+}
+
+}
